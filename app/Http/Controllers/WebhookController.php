9023b7845fd3c55e2656217965bdb7ee359798b6
Elon Musk <admin@Mac-mini-Elon.local>
2017-08-03 16:18:19 +0800

    test bot

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -12,10 +12,24 @@ class WebhookController extends Controller
     {
         $telegram = new Client(config('services.telegram_bot.api_token'), config('services.telegram_bot.api_tracker'));
 
-        $telegram->command('ping', function ($message) use ($telegram) {
+        $telegram->command('start', function ($message) use ($telegram) {
+
+
+
             Log::info('chat_id: ' . $message->getChat()->getId());
         });
 
         $telegram->run();
+    }
+
+    public function send()
+    {
+        $chatId = 439046531;
+
+        $messageText = 'Hello world';
+
+        $bot = new \TelegramBot\Api\BotApi(config('services.telegram_bot.api_token'));
+        
+        $bot->sendMessage($chatId, $messageText);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/send', 'WebhookController@send');
 Route::post('/webhook', 'WebhookController@index');
