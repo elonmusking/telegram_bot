352629d6f9fa04bfa3ea65947ca5b8683bdd89c9
Elon Musk <admin@Mac-mini-Elon.local>
2017-08-03 16:32:41 +0800

    caption

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -13,9 +13,7 @@ class WebhookController extends Controller
         $telegram = new Client(config('services.telegram_bot.api_token'), config('services.telegram_bot.api_tracker'));
 
         $telegram->command('start', function ($message) use ($telegram) {
-            Log::info($message);
-
-            $telegram->sendMessage($message->getChat()->getId());
+            $telegram->sendMessage($message->getChat()->getId(), $message->getCaption());
         });
 
         $telegram->command('check', function ($message) use ($telegram) {
